8ab10a0b20559f448004d9c890f456836717118c
BhushanMandlik <bhushanmandlik2001@gmail.com>
2021-07-19 17:18:37 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    /*int days = 30;
+    String name = "Google";
+    double pi = 3.14;
+    bool isMale = true;
+    num d = 30; //Its takes int as well as double
+
+    var day = 30; //no need to write data type
+    const pi = 3.14; //constant value (data not moodified)
+    //final*/
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello, I am Bhushan Mandlik"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
